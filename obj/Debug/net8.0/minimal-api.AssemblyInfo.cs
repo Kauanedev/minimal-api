213a0cb3c139a4fd1cbd5a6392f11f719d0c5330
Kauane Santos <kauanesantos.coding@gmail.com>
2024-10-26 18:06:07 -0300

    feat: create veiculo table

--- obj/Debug/net8.0/minimal-api.AssemblyInfo.cs
+++ obj/Debug/net8.0/minimal-api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("minimal-api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22c68618fcfd5d6a4e1b66c3f565c1796299c1b9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e99b1ed457903ad2ba86f6856d31a81955eba11a")]
 [assembly: System.Reflection.AssemblyProductAttribute("minimal-api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("minimal-api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
