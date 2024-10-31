6c66314ba2d987af04d3bbe10266ffed817a1a53
Kauane Santos <kauanesantos.coding@gmail.com>
2024-10-31 13:23:44 -0300

    test: get admin by id + fix create admin

--- Test/Domain/Services/AdminServiceTest.cs
+++ Test/Domain/Services/AdminServiceTest.cs
@@ -30,6 +30,7 @@ namespace Test.Domain.Services
 
             return new DbContexto(configuration);
         }
+
         [TestMethod]
         public void TestSaveAdmin()
         {
@@ -39,19 +40,54 @@ namespace Test.Domain.Services
 
             context.Database.ExecuteSqlRaw("TRUNCATE TABLE Administradores;");
 
-            var admin = new Admin();
-
-            admin.Email = "john.doe@example.com";
-            admin.Password = "password123";
-            admin.Perfil = (PerfilEnum)1;
+            var admin = new Admin
+            {
+                Email = "john.doe@example.com",
+                Password = "password123",
+                Perfil = PerfilEnum.Admin
+            };
 
             var adminService = new AdminService(context);
 
             // Act
             adminService.Create(admin);
+            var adminById = adminService.GetById(admin.Id); // obtém o admin pelo ID
 
             // Assert
             Assert.AreEqual(1, adminService.GetAll(1).Count());
+
+            // verificação para garantir que o admin foi criado
+            Assert.IsNotNull(adminById);
+            Assert.AreEqual(admin.Email, adminById.Email); // Verifica se os detalhes estão corretos
+
+            transaction.Rollback(); // Desfaz as alterações feitas no banco de dados
+        }
+
+        [TestMethod]
+        public void TestGetAdminById()
+        {
+            // Arrange
+            var context = CreateTestContext();
+            using var transaction = context.Database.BeginTransaction();
+
+            context.Database.ExecuteSqlRaw("TRUNCATE TABLE Administradores;");
+
+            var admin = new Admin
+            {
+                Email = "john.doe@example.com",
+                Password = "password123",
+                Perfil = PerfilEnum.Admin
+            };
+
+            var adminService = new AdminService(context);
+            adminService.Create(admin); // Certifica que o admin é criado antes de buscá-lo
+
+            // Act
+            var adminById = adminService.GetById(admin.Id); // Passa o ID do admin
+
+            // Assert
+            Assert.IsNotNull(adminById); // Verifica se o admin foi encontrado
+            Assert.AreEqual<string>(admin.Id, adminById.Id); // Verifica se o ID está correto
 
             transaction.Rollback(); // Desfaz as alterações feitas no banco de dados
         }
